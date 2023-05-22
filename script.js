ab566bb8d043ddf98745dace805e45eb6f01b9f3
jyekang <janicekangaroo@gmail.com>
2023-05-22 12:30:38 -0700

    added match cards to disappear

--- script.js
+++ script.js
@@ -88,45 +88,107 @@ const backFaceButtons = [cardObj1, cardObj2, cardObj3, cardObj4, cardObj5, cardO
 
 
 for(let i = 0; i < backFaceButtons.length; i++) {
+    //we starty off with all the buttons w/ backface images
     backFaceButtons[i].element.querySelector('.back-face').src = backFaceButtons[i].backImg;
-    
+    //we're adding an eventListener to btn#. when it's clicked
     backFaceButtons[i].element.addEventListener('click', () => {
         
         // if (backFaceButtons[i].element.disabled) {
         //     return;
         // } else {
+            //the btn# is disabled
             backFaceButtons[i].element.disabled = true;
         // }
-        
-         
 
-        console.log('clicked card');
+        
+        //then, we're changing the backbutton image to the front image
         backFaceButtons[i].element.querySelector('.back-face').src = backFaceButtons[i].frontImg;
     
-    
+        //the number of faceupcards = number of faceupcards +1
         gameState.numberOfFaceUpCards = gameState.numberOfFaceUpCards + 1;
-    
+        
+
+        // -------------*****************************----------------------//
+        
+        //if the number of faceupcards reaches 2 and the front images for first selected and last selected do not match, 
+        //then change the image source to the backface and make sure we can click it again (click disabled is false)
+        //or can we say if first and second do not match (not including frong image)
+
+        //if the number of face cards === 2 and the first and last selected match, remove/hide cards?
+
+        //when all 6 cards are hidden, prompt restart button? alert restart button?
+
+        //.remove .add 
+
+        //shuffling cards
+
+        //when 
+        
+        //shuffle object ; and then render
+        //if the numberof faceup cards is one, then 
         if (gameState.numberOfFaceUpCards === 1) {
-            gameState.firstSelected = backFaceButtons[i].element;
-    
+            gameState.firstSelected = backFaceButtons[i];
+        
             // console.log('first selected is ', backFaceButtons[i].frontImg);
         } else if (gameState.numberOfFaceUpCards === 2) {
-            gameState.lastSelected = backFaceButtons[i]
-    
+            gameState.lastSelected = backFaceButtons[i];
+        
             // console.log('second selected is ', backFaceButtons[i].frontImg)
         }
-    
-    
-        if(gameState.numberOfFaceUpCards === 2 && gameState.firstSelected.frontImg === gameState.lastSelected.frontImg ){
-            // gameState.firstSelected.element.removeEventListener('click')
+
+        if (gameState.numberOfFaceUpCards === 2 && gameState.firstSelected.frontImg === gameState.lastSelected.frontImg) {
+            /** @type {typeof cardObj6 | undefined } */
+            gameState.firstSelected.element.style.opacity = 0; 
+            gameState.lastSelected.element.style.opacity = 0;  
+            gameState.numberOfFaceUpCards = 0
+
             
-            gameState.firstSelected.element.disabled = true;
-            gameState.lastSelected.element.disabled = true;
+            
+            
+            // /** @type {typeof cardObj6 | undefined } */
+            // gameState.lastSelected = backFaceButtons[i].backImg;
+            // backFaceButtons[i].element.disabled = false;
         }
-    });
+
+        // if (gameState.numberOfFaceUpCards === 2 && gameState.firstSelected !== gameState.lastSelected) {
+        //     /** @type {typeof cardObj6 | undefined } */
+        //     gameState.firstSelected = backFaceButtons[i].backImg;
+        //     backFaceButtons[i].element.disabled = false;
+            
+        //     /** @type {typeof cardObj6 | undefined } */
+        //     gameState.lastSelected = backFaceButtons[i].backImg;
+        //     backFaceButtons[i].element.disabled = false;
+        // }
+        //
+
+        
+//@@@@@@@@@ this is where the commented out note section was @@@@@@@@@//
+
+        
+})};
+    
 
 
-}
-// here
+//@@@@@@@@@@@@@@ NOTE FOR ABOVE SECTION @@@@@@@@@@@@@@@@@@@@@@@@@@@//
+// //if the numberof faceup cards is one, then 
+// if (gameState.numberOfFaceUpCards === 1) {
+//     gameState.firstSelected = backFaceButtons[i].element;
+
+//     // console.log('first selected is ', backFaceButtons[i].frontImg);
+// } else if (gameState.numberOfFaceUpCards === 2) {
+//     gameState.lastSelected = backFaceButtons[i]
+
+//     // console.log('second selected is ', backFaceButtons[i].frontImg)
+// }
+
+
+// if(gameState.numberOfFaceUpCards === 2 && gameState.firstSelected.frontImg === gameState.lastSelected.frontImg ){
+//     // gameState.firstSelected.element.removeEventListener('click')
     
+//     gameState.firstSelected.element.disabled = true;
+//     gameState.lastSelected.element.disabled = true;
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//  
     
+
+//do we need to define front image? 
+//what is making the first card chagne?
