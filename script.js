4e7a81742440bf4626458bcf5d5840cadcae2fb1
jyekang <janicekangaroo@gmail.com>
2023-05-25 10:09:34 -0700

    Fixed multiple card flip bug

--- script.js
+++ script.js
@@ -17,13 +17,9 @@ const BACKIMG_URL = './Images/Sun.svg'
 const buttonV2 = [btn1, btn2, btn3, btn4, btn5, btn6]
 const shuffledButtonV2 = [...buttonV2].sort(() => Math.random() - 0.5)
 
-
-
-
 const cardObj1 = {
     backImg: BACKIMG_URL,
     frontImg: './Images/A.svg', 
-    // frontImg: 'https://github.com/dae-hyun-kim/recruit-and-conquer/blob/master/images/archer-intro.gif?raw=true',
     element: shuffledButtonV2[0],
     isFrontImg: 0,
     isBackImg: 1
@@ -71,85 +67,73 @@ const gameState = {
     lastSelected: undefined,
     /** @type {typeof cardObj6 | undefined } */
     firstSelected: undefined,
-
-}
+};
 
 shuffle.addEventListener('click', () => {
     window.location.reload()
 })
 
-//does this need to be here? was it used?
+
 const backFaceButtons = [cardObj1, cardObj2, cardObj3, cardObj4, cardObj5, cardObj6];
-
-// ***********@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*********
-// function called shuffle
-
-// in taht function create an array of all the available indices const arrOfIndices = [0, 1, 2, 3, 4, 5], and an empty arr shufffledIndicesArr []
-
-// for (let i = 0; arrOfIndices.length === 0; i)
-    // randomly pick an index from the array; Math.round(Math.random() * (arrOfIndices.length - 1))   3
-    // then remove that randomly index from arrOfIndices, add it to shufffledIndicesArr [0, 1, 3, 4, 5], [2]
-
-//starting with 0, going through array of backFaceButtons ^ 
-
-//double check where this is used
-const userClickAgain = true;
-
-//**********@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*************
 
 
 for(let i = 0; i < backFaceButtons.length; i++) {
-    //backface buttons (btn#) image source = backImg outlined in object above
+ 
     backFaceButtons[i].element.querySelector('.back-face').src = backFaceButtons[i].backImg;
-    //we're adding an eventListener to btn#. when it's clicked
+  
     backFaceButtons[i].element.addEventListener('click', () => {
-    //the btn# is disabled if it has already been clicked (cant click same button twice and have it register anything)
-    backFaceButtons[i].element.disabled = true;
-    //then, we're changing the backbutton image to the front image
-    backFaceButtons[i].element.querySelector('.back-face').src = backFaceButtons[i].frontImg;
-    
-    //the number of faceupcards = number of faceupcards +1; keeping track of how many cards are faceup
-    gameState.numberOfFaceUpCards = gameState.numberOfFaceUpCards + 1;
+        backFaceButtons[i].element.disabled = true;
+        backFaceButtons[i].element.querySelector('.back-face').src = backFaceButtons[i].frontImg;
+        gameState.numberOfFaceUpCards = gameState.numberOfFaceUpCards + 1;
         
 
-        // -------------*****************************----------------------//
-    
-       
         if (gameState.numberOfFaceUpCards === 1) {
             gameState.firstSelected = backFaceButtons[i];
         
-            // console.log('first selected is ', backFaceButtons[i].frontImg);
         } else if (gameState.numberOfFaceUpCards === 2) {
             gameState.lastSelected = backFaceButtons[i];
-        
-            // console.log('second selected is ', backFaceButtons[i].frontImg)
+            backFaceButtons[0].element.disabled = true;
+            backFaceButtons[1].element.disabled = true;
+            backFaceButtons[2].element.disabled = true;
+            backFaceButtons[3].element.disabled = true;
+            backFaceButtons[4].element.disabled = true;
+            backFaceButtons[5].element.disabled = true;
         }
-      
-      
-        if (gameState.numberOfFaceUpCards === 2 && gameState.firstSelected.frontImg === gameState.lastSelected.frontImg) {
-            setTimeout(()=>{
-                /** @type {typeof cardObj6 | undefined } */
-                gameState.firstSelected.element.style.opacity = 0; 
-                gameState.lastSelected.element.style.opacity = 0;  
-                } , 1000);
-                gameState.numberOfFaceUpCards = 0
-            
-    }
-            
-            // /** @type {typeof cardObj6 | undefined } */
-            // gameState.lastSelected = backFaceButtons[i].backImg;
-            // backFaceButtons[i].element.disabled = false;
         
-//@@@@@@@@@@@@@@@@ if two cards are turned over and front images do not match, change the image back to back image url @@@@@@@@@@@@//
+        setTimeout(()=> {
+            backFaceButtons[i].element.disabled = true;  
+            if (gameState.numberOfFaceUpCards === 2 && gameState.firstSelected.frontImg === gameState.lastSelected.frontImg) {
+            
+                    /** @type {typeof cardObj6 | undefined } */
+                    gameState.firstSelected.element.style.opacity = 0; 
+                    gameState.lastSelected.element.style.opacity = 0;  
+                   
+                    gameState.numberOfFaceUpCards = 0
+                    backFaceButtons[0].element.disabled = false;
+                    backFaceButtons[1].element.disabled = false;
+                    backFaceButtons[2].element.disabled = false;
+                    backFaceButtons[3].element.disabled = false;
+                    backFaceButtons[4].element.disabled = false;
+                    backFaceButtons[5].element.disabled = false;
+                    gameState.firstSelected.element.disabled = true;
+                    gameState.lastSelected.element.disabled = true;
+        }} , 1000);
+
         if (gameState.numberOfFaceUpCards === 2 && gameState.firstSelected.frontImg !== gameState.lastSelected.frontImg) {
+            
             setTimeout(()=>{
              /** @type {typeof cardObj6 | undefined } */
             gameState.firstSelected.element.querySelector('.back-face').src = gameState.firstSelected.backImg;
             gameState.lastSelected.element.querySelector('.back-face').src = gameState.lastSelected.backImg;
-            // gameState.lastSelected.frontImg = gameState.lastSelected.backImg
             gameState.firstSelected.element.disabled = false;
             gameState.lastSelected.element.disabled = false;
-            }, 1500);
+            backFaceButtons[0].element.disabled = false;
+            backFaceButtons[1].element.disabled = false;
+            backFaceButtons[2].element.disabled = false;
+            backFaceButtons[3].element.disabled = false;
+            backFaceButtons[4].element.disabled = false;
+            backFaceButtons[5].element.disabled = false;
+            }, 1000);
             gameState.numberOfFaceUpCards = 0;
        
     }
@@ -161,7 +145,6 @@ for(let i = 0; i < backFaceButtons.length; i++) {
         let counter = 0
     })
 
-       //@@@@@@@@@@@ //check if all card opacity === 0 @@@@@@@@@@//
        setTimeout(()=>{
        
        let counter = 0
@@ -172,29 +155,11 @@ for(let i = 0; i < backFaceButtons.length; i++) {
            }
         }
 
-        //we have to check that the number in counter is the same number of the cards we have (.length)
-
         if(counter == backFaceButtons.length) {
             alert("You Win!")
         }
     }, 2000);
     })}
-
-       //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-        // if (gameState.numberOfFaceUpCards === 2 && gameState.firstSelected !== gameState.lastSelected) {
-        //     /** @type {typeof cardObj6 | undefined } */
-        //     gameState.firstSelected = backFaceButtons[i].backImg;
-        //     backFaceButtons[i].element.disabled = false;
-            
-        //     /** @type {typeof cardObj6 | undefined } */
-        //     gameState.lastSelected = backFaceButtons[i].backImg;
-        //     backFaceButtons[i].element.disabled = false;
-        // }
-        //
-
-
-        
-
 
 
 
